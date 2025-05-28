14752b7208aec7331e1d22cda81414671665a832
johanapollo <johan.vilchez@apollo.io>
2025-05-28 14:25:20 -0700

    Add files via upload

---
+++ Vendor.js
@@ -0,0 +1,4 @@
+export * as THREE from 'https://unpkg.com/three@0.150.1/build/three.module.js';
+export { GLTFLoader } from 'https://unpkg.com/three@0.150.1/examples/jsm/loaders/GLTFLoader.js';
+export { OrbitControls } from 'https://unpkg.com/three@0.150.1/examples/jsm/controls/OrbitControls.js';
+export * as CANNON from 'https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/dist/cannon-es.js';
